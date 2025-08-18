f817e6cee809338a599eeebbc067f669237b26c6
Garou2 <amanmahish73@gmail.com>
2025-08-18 17:21:35 +0530

    fixed commands

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -16,11 +16,12 @@ const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 (async () => {
   try {
     console.log("Started refreshing application (/)commands ");
-    await rest.put(Routes.applicationCommand(process.env.CLIENT_ID), {
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
       body: commands,
     });
     console.log("Successfully reloaded application (/) commands.");
+   
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
   }
 })();
