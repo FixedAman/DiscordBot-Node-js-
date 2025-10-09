4b0d31d1ce957a6d1184fcf93a5b1936743c3758
FixedAman <amanmahish73@gmail.com>
2025-10-09 21:42:02 +0530

    adding error section to handle command errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-const { Client, GatewayIntentBits, Collection, Events } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  Events,
+  MessageFlags,
+} = require("discord.js");
 require("dotenv").config();
 const fs = require("node:fs");
 const path = require("node:path");
@@ -48,9 +54,30 @@ client.on("messageCreate", (message) => {
   }
 });
 // setting interaction
-client.on(Events.InteractionCreate, (interaction) => {
+client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found`);
+    return;
+  }
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command !",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
   console.log(interaction);
 });
-// fixing gitsss 
+// fixing gitsss
 client.login(process.env.BOT_TOKEN);
